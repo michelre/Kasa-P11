5300805a84be4b5cdaa4bc3e9d9d28d60ea8ac38
Rémi Michel <remi.michel38@gmail.com>
2023-11-02 17:24:26 +0100

    feat(): Session du 02/11

--- kasa/src/components/Accordeon.jsx
+++ kasa/src/components/Accordeon.jsx
@@ -1,7 +1,12 @@
+import {useState} from 'react'
+
 const Accordeon = ({title, children}) => {
+
+    const [display, setDisplay] = useState(false)
+
     return <div>
-        <div>{title}</div>
-        {children}
+        <button onClick={() => setDisplay(!display)}>{title}</button>
+        <div style={{display: display ? 'block' : 'none'}}>{children}</div>
     </div>
 }
 
--- kasa/src/components/Stars.jsx
+++ kasa/src/components/Stars.jsx
@@ -4,21 +4,32 @@ import emptyStar from '../assets/empty_star.svg';
 
 // Ajouter un call sur le fichier JSON pour récupérer les datas RATING
 
-const Stars = ({rating}) => {
+/*const Stars = ({rating}) => {
     const stars = []
-        for (let i = 0; i < 5; i++) {
-            if (i < rating) {
-                stars.push(<img src={fullStar} alt='full-star' key={i} />)
-            } else {
-            stars.push(<img src={emptyStar} alt='empty-star' key={i} />)
-            }
+    for (let i = 0; i < 5; i++) {
+        if (i < rating) {
+            stars.push(<img src={fullStar} alt='full-star' key={i}/>)
+        } else {
+            stars.push(<img src={emptyStar} alt='empty-star' key={i}/>)
         }
+    }
 
-        return (
+    return (
         <div className='rating'>
             {stars}
         </div>
     );
-};
+};*/
+
+
+const Stars = ({rating}) => {
+
+    return <div className='rating'>
+        {Array.from({length: 5}).map((e, idx) => idx < rating ?
+            <img src={fullStar} alt='full-star' key={idx}/> :
+            <img src={emptyStar} alt='empty-star' key={idx}/>)}
+    </div>
+
+}
 
 export default Stars;
