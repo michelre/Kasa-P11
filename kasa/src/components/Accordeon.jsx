4f6cac9e039f56a01ec69556709327cbff87ad4a
Graff Médéric <graffmederic@gmail.com>
2023-11-09 18:34:34 +0100

    Composant Accorderon : Ajout Datas page About + Init mise en forme CSS

--- kasa/src/components/Accordeon.jsx
+++ kasa/src/components/Accordeon.jsx
@@ -1,13 +1,24 @@
 import {useState} from 'react'
+import UpArrow from "../assets/chevron-up.png";
+import DownArrow from "../assets/chevron-down.png";
 
 const Accordeon = ({title, children}) => {
 
     const [display, setDisplay] = useState(false)
 
-    return <div>
-        <button onClick={() => setDisplay(!display)}>{title}</button>
-        <div style={{display: display ? 'block' : 'none'}}>{children}</div>
-    </div>
+    return <div className='accordeon'>
+                <div className="according-container">
+                    <span className='accordeon-header' onClick={() => setDisplay(!display)}>{title}
+                        {display === title ? (
+                                <img src={UpArrow} alt="uparrow" />
+                            ) : (
+                                <img src={DownArrow} alt="downarrow" />
+                        )}
+                    </span>
+                    <div className='accordeon-description' style={{display: display ? 'block' : 'none'}}>{children}</div>
+                </div>
+            </div>
 }
+
 
 export default Accordeon;
--- kasa/src/pages/About.jsx
+++ kasa/src/pages/About.jsx
@@ -1,6 +1,6 @@
 import Header from "../components/Header.jsx";
 import Banner from '../components/Banner.jsx';
-import Tags from "../components/Tags.jsx";
+import Accordeon from "../components/Accordeon.jsx";
 import Footer from "../components/Footer.jsx";
 
 const About = () => {
@@ -9,9 +9,32 @@ const About = () => {
         <Banner
             image={'../src/assets/about_banner.jpg'}
         />
-        <Tags/>
+
+        <div className='about-container'>
+            <Accordeon
+                title={'Fiabilité'}
+            >
+                <p>{`Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.`}</p>
+            </Accordeon>
+            <Accordeon
+                title={'Respect'}
+            >
+                <p>{'La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.'}</p>
+            </Accordeon>
+            <Accordeon
+                title={'Service'}
+            >
+                <p>{'Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N"hésitez pas à nous contacter si vous avez la moindre question.'}</p>
+            </Accordeon>
+            <Accordeon
+                title={'Sécurité'}
+            >
+                <p>{'La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l"hôte qu"au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.'}</p>
+            </Accordeon>
+        </div>
+
         <Footer/>
     </div>
-}
+} 
 
 export default About
